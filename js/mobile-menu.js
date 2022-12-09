16a2861f6d3b40ff2b6392912589416dbf983944
Przemyslaw-Grzybowski <pe.grzybowski@gmail.com>
2022-12-09 02:31:43 +0100

    first draft mobile window

---
+++ js/mobile-menu.js
@@ -0,0 +1,11 @@
+const menuOverlay = document.querySelector(".mobile-menu");
+const closeMenuButton = document.querySelector(".close-menu");
+const openMenuBtn = document.querySelector(".open-menu");
+
+openMenuBtn.addEventListener("click", (e) => {
+    menuOverlay.classList.remove("d-none");
+});
+
+closeMenuButton.addEventListener("click", (e) => {
+    menuOverlay.classList.add("d-none");
+});
